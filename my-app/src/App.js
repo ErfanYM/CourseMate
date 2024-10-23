097329c489b3bf6b00a16a1f5a4a68a3037cf138
adilgulu <ad.guluzade@gmail.com>
2024-10-23 12:46:32 -0400

    Removed the React app logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <div>
-        <h1>Welcome to our React App!</h1>
-        <p>This is our custom HTML page.</p>
-      </div>
-
+      <h1>Welcome to My React App!</h1>
+      <p>This is my custom content.</p>
     </div>
-    
   );
 }
-
 export default App;
