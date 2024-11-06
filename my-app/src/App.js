8555a3e4f8ebc908d8ad0f8b01716593919c3639
ErfanYM <yosefyerfan1@gmail.com>
2024-11-06 13:05:01 -0500

    Changes on Dashboard menu and add Courses/Home and Calendar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Courses from './components/Courses';
 import Calendar from './components/Calendar';
@@ -19,17 +20,31 @@ function App() {
   }
 
   return (
-    <div>
+    <Router>
       <Header />
       <div className="main-container">
-        <section className="courses-section">
-          <Courses courses={courses} addCourse={addCourse} deleteCourse={deleteCourse}/>
-        </section>
-        <section className="calendar-section">
-          <Calendar />
-        </section>
+        <Routes>
+        <Route
+            path="/"
+            element={
+              <>
+                <section className="courses-section">
+                  <Courses courses={courses} addCourse={addCourse} deleteCourse={deleteCourse} />
+                </section>
+                <section className="calendar-section">
+                  <Calendar />
+                </section>
+              </>
+            }
+          />
+        <Route
+            path="/courses"
+            element={<Courses courses={courses} addCourse={addCourse} deleteCourse={deleteCourse} />}
+          />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
